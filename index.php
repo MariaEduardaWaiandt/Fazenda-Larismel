01ee9dce924c43fb2efb125bd8d30c4c3574ac23
MariaEduardaWaiandt <mariaeduardawaiandt@gmail.com>
2025-04-04 08:29:06 -0400

    criação dos arquivos index.php e layout.css

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/layout.css">
+    <title>Fazenda Larismel - Cria e Recria de Gado de Corte</title>
+</head>
+
+<body>
+    <!--nosso cabeçalho -->
+    <header>
+        <h1>Fazenda Larismel</h1>
+        <p>Especializada em Cria e Recria de Gado de Corte</p>
+    </header>
+    <!--menu de navegação-->
+    <nav>
+        <a href="#sobre">Sobre</a>
+        <a href="#servicos">Atividades</a>
+        <a href="#estrutura">Nossa Estrutura</a>
+        <a href="#contato">Contato</a>
+    </nav>
+    <main>
+        <div class="dest">
+            Qualidade e Sustentabilidade na Produção de Gado de Corte!
+        </div>
+        <section id="sobre">
+            <h2>Sobre Nós</h2>
+            <p>Somos uma fazenda referência na criação e recria de gado de corte, utilizando práticas sustentaveis e manejo de alta qualidade.</p>
+        </section>
+        <section id="servicos">
+            <h2>Atividades</h2>
+            <ul>
+                <li>Seleção Genética e Melhorando</li>
+                <li>Manejo Nutricional para Cria e Recria</li>
+                <li>Sanidade Animal e Bem-Estar</li>
+                <li>Integração Lavoura_Pecuária</li>
+            </ul>
+        </section id= "estrutura">
+           <h2>Nossa Estrutura</h2>
+           <p>Contamos com pastagens de qualidade, curral moderno, áreas de confinamento e gestão eficiente do rebanho.</p>
+    </section>
+
+    <section id="contato">
+        <h2>Contato</h2>
+        <p>Email: contato@fazendaLarismel.com</p>
+        <p>Telefone: (69)99304-3021</p>
+    </section>
+    </main>
+    <footer> 
+        <p>&copy; 2025 Fazenda Larismel - Todos os direitos reservados.</p>
+    </footer>
+</body>
+
+</html>
